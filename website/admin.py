d4d7813c51409bff04541f43a3196b9da14ec6d5
Kamil Warchoł <kamil.warchol.93@gmail.com>
2018-04-22 21:00:43 +0200

    Modify admin site

--- website/admin.py
+++ website/admin.py
@@ -1,6 +1,35 @@
 from django.contrib import admin
+from .models import Client, Contractor, Business, BusinessType, Event
 
-from .models import Client
+
+class EventInline(admin.TabularInline):
+    model = Event
+    extra = 1
 
 
-admin.site.register(Client)
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    inlines = [
+        EventInline,
+    ]
+    list_display = ['user']
+
+
+@admin.register(Contractor)
+class ContractorAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Business)
+class BusinessAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(BusinessType)
+class BusinessTypeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    pass
