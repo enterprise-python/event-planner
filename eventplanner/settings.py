d8652c10da1d1ca05c26709c7cfe1860b41e6aff
Piotr Woś <ptr97@users.noreply.github.com>
2018-04-23 17:36:17 +0200

    Add login/logout functionality

--- eventplanner/settings.py
+++ eventplanner/settings.py
@@ -1,6 +1,7 @@
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
@@ -101,3 +102,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+LOGIN_REDIRECT_URL = 'website:index'
+LOGIN_URL = 'website:login'
+LOGOUT_REDIRECT_URL = 'website:index'
