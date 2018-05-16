74dd6daff70e6346f8a94f08bf2c709c90a9765f
Piotr Woś <ptr97@users.noreply.github.com>
2018-05-16 16:28:07 +0200

    Add password change

--- website/urls.py
+++ website/urls.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import views as auth_views
-from django.urls import path
+from django.urls import path, reverse_lazy
 from django.views.generic import TemplateView
 
 from . import views
@@ -18,4 +18,13 @@ urlpatterns = [
     path('logout/',
          auth_views.LogoutView.as_view(template_name='website/login.html'),
          name='logout'),
+    path('change-password/',
+         auth_views.PasswordChangeView.as_view(
+             template_name='website/change_password.html',
+             success_url=reverse_lazy('website:password_change_done')),
+         name='change_password'),
+    path('change-password/done/',
+         auth_views.PasswordChangeDoneView.as_view(
+             template_name='website/password_change_done.html'),
+         name='password_change_done'),
 ]
