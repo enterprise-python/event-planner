b504340c3ce86005d532e9a929c908bf34debe8c
Piotr Woś <ptr97@users.noreply.github.com>
2018-05-20 22:57:16 +0200

    Add profile edit url

--- website/urls.py
+++ website/urls.py
@@ -6,17 +6,20 @@ from . import views
 
 app_name = 'website'
 urlpatterns = [
-    path('', TemplateView.as_view(template_name="website/index.html"),
+    path('', TemplateView.as_view(template_name="website/pages/index.html"),
          name='index'),
     path('register-client/', views.ClientRegistrationView.as_view(),
          name='register_client'),
     path('register-contractor/', views.ContractorRegistrationView.as_view(),
          name='register_contractor'),
     path('login/',
-         auth_views.LoginView.as_view(template_name='website/login.html'),
+         auth_views.LoginView.as_view(template_name='website/pages/login.html'),
          name='login'),
     path('logout/',
-         auth_views.LogoutView.as_view(template_name='website/login.html'),
+         auth_views.LogoutView.as_view(template_name='website/pages/login.html'),
          name='logout'),
-    path('edit/', views.ClientEditView.as_view(), name='edit'),
+    path('profile/', TemplateView.as_view(
+        template_name="website/pages/profile.html"),
+         name='profile'),
+    path('profile/edit/', views.ClientEditView.as_view(), name='edit')
 ]
