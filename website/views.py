2b72c0702dae08387b598dc4d2e50afa54d8766f
Kamil Warchoł <kamil.warchol.93@gmail.com>
2018-05-27 21:49:03 +0200

    Refactor views.py

--- website/views.py
+++ website/views.py
@@ -1,14 +1,15 @@
 from django.contrib import messages
 from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth.forms import PasswordChangeForm
-from django.http import HttpResponseRedirect, Http404
+from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
-from django.views.generic import View, ListView, DetailView
+from django.views.generic import DetailView, ListView, View
 
+from .forms import (BusinessForm, ClientCreationForm, ClientEditForm,
+                    ContractorCreationForm, ContractorEditForm, EventForm,
+                    UserCreationForm, UserEditForm)
 from website.models import Role, Business, Event
-from .forms import ClientCreationForm, UserCreationForm, ContractorCreationForm, \
-    UserEditForm, ClientEditForm, ContractorEditForm, EventForm, BusinessForm
 
 
 class ClientRegistrationView(View):
@@ -17,12 +18,9 @@ class ClientRegistrationView(View):
     template_name = 'website/pages/register_client.html'
 
     def get(self, request):
-        user_form = self.user_form(None)
-        client_form = self.client_form(None)
-
         return render(request, self.template_name, {
-            'user_form': user_form,
-            'client_form': client_form
+            'user_form': self.user_form(None),
+            'client_form': self.client_form(None)
         })
 
     def post(self, request):
@@ -52,12 +50,9 @@ class ContractorRegistrationView(View):
     template_name = 'website/pages/register_contractor.html'
 
     def get(self, request):
-        user_form = self.user_form(None)
-        contractor_form = self.contractor_form(None)
-
         return render(request, self.template_name, {
-            'user_form': user_form,
-            'contractor_form': contractor_form
+            'user_form': self.user_form(None),
+            'contractor_form': self.contractor_form(None)
         })
 
     def post(self, request):
@@ -85,51 +80,49 @@ class ProfileEditView(View):
     user_edit_form = UserEditForm
     client_edit_form = ClientEditForm
     contractor_edit_form = ContractorEditForm
-    change_password_form = PasswordChangeForm
+    password_change_form = PasswordChangeForm
     template_name = "website/pages/edit_profile.html"
 
     def get(self, request):
         if request.user.is_client():
-            return self.get_client(request)
+            return self._get_client(request)
         elif request.user.is_contractor():
-            return self.get_contractor(request)
+            return self._get_contractor(request)
 
-    def get_client(self, request):
+    def _get_client(self, request):
         user_edit_form = self.user_edit_form(instance=request.user)
         client_edit_form = self.client_edit_form(instance=request.user)
-        change_password_form = self.change_password_form(request.user)
+        change_password_form = self.password_change_form(user=request.user)
 
-        return render(request, self.template_name,
-                      {
-                          'user_edit_form': user_edit_form,
-                          'client_edit_form': client_edit_form,
-                          'change_password_form': change_password_form
-                      })
+        return render(request, self.template_name, {
+            'user_edit_form': user_edit_form,
+            'client_edit_form': client_edit_form,
+            'change_password_form': change_password_form
+        })
 
-    def get_contractor(self, request):
+    def _get_contractor(self, request):
         user_edit_form = self.user_edit_form(instance=request.user)
         contractor_edit_form = self.contractor_edit_form(instance=request.user)
-        change_password_form = self.change_password_form(request.user)
+        change_password_form = self.password_change_form(request.user)
 
-        return render(request, self.template_name,
-                      {
-                          'user_edit_form': user_edit_form,
-                          'contractor_edit_form': contractor_edit_form,
-                          'change_password_form': change_password_form
-                      })
+        return render(request, self.template_name, {
+            'user_edit_form': user_edit_form,
+            'contractor_edit_form': contractor_edit_form,
+            'change_password_form': change_password_form
+        })
 
     def post(self, request):
         if request.user.is_client():
-            return self.post_client(request)
+            return self._post_client(request)
         elif request.user.is_contractor():
-            return self.post_contractor(request)
+            return self._post_contractor(request)
 
-    def post_client(self, request):
+    def _post_client(self, request):
         user_edit_form = self.user_edit_form(request.POST,
                                              instance=request.user)
         client_edit_form = self.client_edit_form(request.POST,
                                                  instance=request.user)
-        change_password_form = self.change_password_form(request.user,
+        change_password_form = self.password_change_form(request.user,
                                                          request.POST)
 
         if user_edit_form.is_valid() and client_edit_form.is_valid():
@@ -148,19 +141,18 @@ class ProfileEditView(View):
 
         messages.error(request, 'Please correct the error below.')
 
-        return render(request, self.template_name,
-                      {
-                          'user_edit_form': user_edit_form,
-                          'client_edit_form': client_edit_form,
-                          'change_password_form': change_password_form
-                      })
+        return render(request, self.template_name, {
+            'user_edit_form': user_edit_form,
+            'client_edit_form': client_edit_form,
+            'change_password_form': change_password_form
+        })
 
-    def post_contractor(self, request):
+    def _post_contractor(self, request):
         user_edit_form = self.user_edit_form(request.POST,
                                              instance=request.user)
         contractor_edit_form = self.contractor_edit_form(request.POST,
                                                          instance=request.user)
-        change_password_form = self.change_password_form(request.user,
+        change_password_form = self.password_change_form(request.user,
                                                          request.POST)
 
         if user_edit_form.is_valid() and contractor_edit_form.is_valid():
@@ -178,13 +170,11 @@ class ProfileEditView(View):
             return HttpResponseRedirect(reverse('website:edit'))
 
         messages.error(request, 'Please correct the error below.')
-
-        return render(request, self.template_name,
-                      {
-                          'user_edit_form': user_edit_form,
-                          'contractor_edit_form': contractor_edit_form,
-                          'change_password_form': change_password_form
-                      })
+        return render(request, self.template_name, {
+            'user_edit_form': user_edit_form,
+            'contractor_edit_form': contractor_edit_form,
+            'change_password_form': change_password_form
+        })
 
 
 class EventsListView(ListView):
@@ -196,7 +186,7 @@ class EventsListView(ListView):
             raise Http404()
 
         return Event.objects.filter(owner=self.request.user.client).order_by(
-            'date_from')
+            '-date_from')
 
 
 class EventDetailView(DetailView):
@@ -240,7 +230,7 @@ class AddEventView(View):
             return HttpResponseRedirect(reverse('website:events'))
 
         return render(request, self.template_name, {
-            'event_form': self.event_form
+            'event_form': event_form
         })
 
 
@@ -248,29 +238,21 @@ class EditEventView(View):
     event_form = EventForm
     template_name = 'website/pages/edit_event.html'
 
-    @staticmethod
-    def _check_event_owner(request, pk):
-        if not request.user.is_client():
-            raise Http404()
-
-        for event in Event.objects.filter(owner=request.user.client):
-            if event.pk == pk:
-                break
-        else:
+    def _check_event_owner(self, user, pk):
+        if not (user.is_client()
+                and Event.objects.get(pk=pk) in user.client.event_set):
             raise Http404()
 
     def get(self, request, pk):
-        self._check_event_owner(request, pk)
-
+        self._check_event_owner(request.user, pk)
         return render(request, self.template_name, {
-            'event_form': self.event_form(instance=Event.objects.filter(pk=pk))
+            'event_form': self.event_form(instance=Event.objects.get(pk=pk))
         })
 
     def post(self, request, pk):
-        self._check_event_owner(request, pk)
-
+        self._check_event_owner(request.user, pk)
         event_form = self.event_form(request.POST,
-                                     instance=Event.objects.filter(pk=pk))
+                                     instance=Event.objects.get(pk=pk))
         if event_form.is_valid():
             event_form.save()
             messages.success(request, 'Your event was successfully updated!')
@@ -317,31 +299,25 @@ class EditBusinessView(View):
     business_form = BusinessForm
     template_name = 'website/pages/edit_business.html'
 
-    @staticmethod
-    def _check_business_owner(request, pk):
-        if not request.user.is_contractor():
-            raise Http404()
-
-        for business in Business.objects.filter(owner=request.user.client):
-            if business.pk == pk:
-                break
-        else:
+    def _check_business_owner(self, user, pk):
+        if not (user.is_contractor()
+                and Business.objects.get(pk=pk)
+                in user.contractor.business_set):
             raise Http404()
 
     def get(self, request, pk):
-        self._check_business_owner(request, pk)
+        self._check_business_owner(request.user, pk)
 
+        business_form = self.business_form(instance=Business.objects.get(pk=pk))
         return render(request, self.template_name, {
-            'business_form': self.business_form(
-                instance=Business.objects.filter(pk=pk))
+            'business_form': business_form
         })
 
     def post(self, request, pk):
-        self._check_business_owner(request, pk)
+        self._check_business_owner(request.user, pk)
 
         business_form = self.business_form(request.POST,
-                                           instance=Business.objects.filter(
-                                               pk=pk))
+                                           instance=Business.objects.get(pk=pk))
         if business_form.is_valid():
             business_form.save()
             messages.success(request, 'Your business was successfully updated!')
