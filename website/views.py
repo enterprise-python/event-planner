715f775566f97886bb99104febbc31a0bda23256
przszo <prz.szo@gmail.com>
2018-05-30 22:31:15 +0200

    Add error messages in login and register pages.

--- website/views.py
+++ website/views.py
@@ -15,7 +15,7 @@ from .forms import ClientCreationForm, UserCreationForm, ContractorCreationForm,
 class ClientRegistrationView(View):
     user_form = UserCreationForm
     client_form = ClientCreationForm
-    template_name = 'website/pages/register_client.html'
+    template_name = 'website/pages/register.html'
 
     def get(self, request):
         user_form = self.user_form(None)
@@ -50,7 +50,7 @@ class ClientRegistrationView(View):
 class ContractorRegistrationView(View):
     user_form = UserCreationForm
     contractor_form = ContractorCreationForm
-    template_name = 'website/pages/register_contractor.html'
+    template_name = 'website/pages/register.html'
 
     def get(self, request):
         user_form = self.user_form(None)
